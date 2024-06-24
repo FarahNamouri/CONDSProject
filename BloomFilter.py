2d815825348b3fd8923df1e12f71fedee0d44cdc
FaisalSherKaizer <faisalsher801@gmail.com>
2024-06-24 00:56:01 +0000

    Update BloomFilter.py
    insert():function to added elements to bloom filter has been created.

--- BloomFilter.py
+++ BloomFilter.py
@@ -28,7 +28,18 @@ class BloomFilter:
         return int(n)
     
     def insert(self, data):
-        pass
+        if self.verify(data)==False:
+            position_pointer=0
+            for i in range(self.hash_num):
+                if i%2==0:
+                    position_pointer=int(mmh3.hash128(data, i,False)%int(self.length))
+                    self.bit_vector[position_pointer]=True
+                else:
+                    position_pointer=int(pbkdf2_hmac('sha256', data.encode('utf-8'), str(i).encode('utf-8') * 2, 500_000).hex(),16)%int(self.length)
+                    self.bit_vector[position_pointer]=True
+            print('The Input definatly did not already exists, hence it has been added')
+        else:
+            print('The Input probably already exists')
 
     def verify(self, data):
         pass

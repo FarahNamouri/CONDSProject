22671554f457d18b437073b30a10d39137aa2455
FaisalSherKaizer <faisalsher801@gmail.com>
2024-06-24 20:39:58 +0000

    Test the performance of insert function
    This function is to calculate the performance the average time taken for inserting DNA sequences. It also generates a graph.

---
+++ insert_test_performance.py
@@ -0,0 +1,63 @@
+#!usr/bin/env/ pyhton
+import math
+import mmh3
+from hashlib import sha256
+import hmac,hashlib,io
+from hashlib import pbkdf2_hmac
+from bitarray import bitarray
+from BloomFilter import BloomFilter
+import numpy as np
+import random
+from wonderwords import RandomSentence,RandomWord
+import time
+import matplotlib.pyplot as plt
+
+def generate_words(n=0):
+    words=[]
+    for i in range(n):
+        r = RandomWord()
+        words.append(r.word())
+    return set(words)
+
+def main():
+    print('insert')
+    data=generate_words(1000)
+    data_list = list(data)
+
+    sizes = [10, 50,75, 100,200,350, 500, 1_000,5_000]
+
+    samples = [
+        random.sample(data_list, k=size) for size in sizes
+    ]
+
+    nr_runs = 3
+    times = {}
+    insert_sample = random.sample(data_list, k=20)
+    i=0
+
+
+
+    for sample in samples:
+        btree = BloomFilter(sizes[i],0.05)
+        i+=1
+        for word in sample:
+            btree.insert(word)
+        times[len(sample)] = 0.0
+        for _ in range(nr_runs):
+            start_time = time.time_ns()
+            for word in insert_sample:
+                btree.insert(word)
+            end_time = time.time_ns()
+            times[len(sample)] += end_time - start_time
+        times[len(sample)] /= nr_runs*1_000_000.0
+        print(times)
+
+    plt.plot(times.keys(), times.values());
+
+    plt.savefig('plotinsert.png')
+
+
+if __name__ == "__main__":
+    main()
+
+

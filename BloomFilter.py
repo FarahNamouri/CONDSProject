956f1a332e80d3968fdbb7df20e79bf5902dcd7f
FaisalSherKaizer <faisalsher801@gmail.com>
2024-06-24 00:53:16 +0000

    Add files via upload
    Bloom filter skeleton code. Only constructor and function names present.

--- BloomFilter.py
+++ BloomFilter.py
@@ -18,51 +18,21 @@ class BloomFilter:
             self.bit_vector=int(self.length)*bitarray('0')
         except ValueError as exp:
             print(f"An exception of the type {exp} has occured")
-        #print(self.length)
     
     @classmethod
     def calc_length(self, n1, n2): 
-        n = (-(n1*math.log(n2))/(math.log(2))**2) 
-        return n
+        pass
     
     @classmethod
     def optimum_hashes(self, n1, n2): 
-        n=((n1/n2)*math.log(2))
-        return int(n)
+        pass
     
     def insert(self, data):
-        if self.verify(data)==False:
-            position_pointer=0
-            for i in range(self.hash_num):
-                if i%2==0:
-                    position_pointer=int(mmh3.hash128(data, i,False)%int(self.length))
-                    self.bit_vector[position_pointer]=True
-                else:
-                    #position_pointer=int(sha256(data.encode('utf-8')).hexdigest()),16)%self.length
-                    position_pointer=int(pbkdf2_hmac('sha256', data.encode('utf-8'), str(i).encode('utf-8') * 2, 500_000).hex(),16)%int(self.length)
-                    self.bit_vector[position_pointer]=True
-            print('The Input definatly did not already exists, hence it has been added')
-        else:
-            print('The Input probably already exists')
+        pass
 
     def verify(self, data):
-        position_pointer=0
-        for i in range(self.hash_num):
-            if i%2==0:
-                position_pointer=int(mmh3.hash128(data, i,False)%int(self.length))
-                
-                if self.bit_vector[position_pointer]==1:
-                    pass
-                else:
-                    return False
-            else:
-                position_pointer=int(pbkdf2_hmac('sha256', data.encode('utf-8'), str(i).encode('utf-8') * 2, 500_000).hex(),16)%int(self.length)
-                if self.bit_vector[position_pointer]==1:
-                    pass
-                else:
-                    return False
-        return True
+        pass
     
     def __str__(self):  
-        return "The bloom filter in string form is % s" % (self.bit_vector) 
+        pass
                     

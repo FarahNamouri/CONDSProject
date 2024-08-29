2abefdad7eef7430576e7235a4e950b863e7e222
FarahNamouri <110462179+FarahNamouri@users.noreply.github.com>
2024-08-29 06:45:05 +0200

    improved variables' names

--- BloomFilter.py
+++ BloomFilter.py
@@ -1,4 +1,4 @@
-#libraries
+# importing necessary libraries
 from BitVector import BitVector
 import math
 import mmh3
@@ -18,7 +18,7 @@ class BloomFilter:
         '''
         self.length=self.calc_length(expected_num, prob)
         self.prob=prob
-        self.hash_num=self.optimum_hashes(self.length ,expected_num)
+        self.hash_num=self.optimum_hashes(self.length ,expected_num) # number of hashes
         self.C=self.compute_cr(self.length, expected_num)
         try:
             self.bit_vector=int(self.length)*bitarray('0')
@@ -42,52 +42,50 @@ class BloomFilter:
         return m/expected_num
     
     @classmethod
-    def optimum_hashes(self, n1, n2):
+    def optimum_hashes(self, item1, item2):
         '''
         calculating number of hash functions to use
         '''
-        n=((n1/n2)*math.log(2))
-        return int(n)
+        hash_number=((item1/item2)*math.log(2))
+        return int(hash_number)
     
     def insert(self, data):
         '''
-        adding item to the bloom filter
+        adding an item to the bloom filter
         '''
         if type(data) is int:
-            data=str(data)
-        if self.verify(data)==False:
-            position_pointer=0
+            data = str(data)
+        if self.verify(data) == False:
+            position_pointer = 0
             for i in range(self.hash_num):
-                if i%2==0:
-                    position_pointer=int(mmh3.hash128(data, i,False)%int(self.length))
-                    self.bit_vector[position_pointer]=True
+                if i%2 == 0:
+                    position_pointer = int(mmh3.hash128(data, i, False)%int(self.length))
+                    self.bit_vector[position_pointer] = True
                 else:
-                    position_pointer=int(pbkdf2_hmac('sha256', data.encode('utf-8'), str(i).encode('utf-8') * 2, 500_000).hex(),16)%int(self.length)
-                    self.bit_vector[position_pointer]=True
-            print('The Input definatly did not already exists, hence it has been added')
+                    position_pointer = int(pbkdf2_hmac('sha256', data.encode('utf-8'), str(i).encode('utf-8') * 2, 500_000).hex(), 16)%int(self.length)
+                    self.bit_vector[position_pointer] = True
+            print('The Input didn't exist already, hence it has been added.')
         else:
-            print('The Input probably already exists')
+            print('The Input probably already exists.')
 
     def verify(self, data):
         '''
         verifying if an element is in the bloom filter
         '''
         if type(data) is int:
-            data=str(data)
-        position_verify=[None]*int(self.length)
-        position_pointer=0
+            data = str(data)
+        position_verify = [None]*int(self.length)
+        position_pointer = 0
         for i in range(self.hash_num):
-            if i%2==0:
-                position_pointer=int(mmh3.hash128(data, i,False)%int(self.length))
-                
-                if self.bit_vector[position_pointer]==1:
+            if i%2 == 0:
+                position_pointer = int(mmh3.hash128(data, i, False)%int(self.length))   
+                if self.bit_vector[position_pointer] == 1:
                     pass
                 else:
                     return False
             else:
-                #position_pointer=int(sha256(data.encode('utf-8')).hexdigest()),16)%self.length
-                position_pointer=int(pbkdf2_hmac('sha256', data.encode('utf-8'), str(i).encode('utf-8') * 2, 500_000).hex(),16)%int(self.length)
-                if self.bit_vector[position_pointer]==1:
+                position_pointer = int(pbkdf2_hmac('sha256', data.encode('utf-8'), str(i).encode('utf-8') * 2, 500_000).hex(),16)%int(self.length)
+                if self.bit_vector[position_pointer] == 1:
                     pass
                 else:
                     return False

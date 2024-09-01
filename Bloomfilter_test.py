faff454527e6eb637b06a526ab706d02bd9b020a
FarahNamouri <110462179+FarahNamouri@users.noreply.github.com>
2024-09-01 16:05:24 +0200

    Updated Bloomfilter_test.py
    Modified the format of the code to make it more pythonic.

--- Bloomfilter_test.py
+++ Bloomfilter_test.py
@@ -16,35 +16,35 @@ import matplotlib.pyplot as plt
 np.random.seed(0)
 random.seed(0)
 
-length=20
-probability=0.05
-max_size=5
+length = 20
+probability = 0.05
+max_size = 5
 
 def generate_number(length):
     lst=[]
     for i in range(length):
-        lst.append(random.randrange(1,100000))
+        lst.append(random.randrange(1, 100000))
     return lst
 
-def generate_number2(length,lst_prior):
-    lst=[]
+def generate_number2(length, lst_prior):
+    lst = []
     for i in range(length):
-        temp=(random.randrange(1,100000))
+        temp = (random.randrange(1, 100000))
         if temp not in (lst_prior):
             lst.append(temp)
     return lst
 
-num1=(generate_number(length))
+num1 = (generate_number(length))
 
-num2=(generate_number2(length,num1))
+num2 = (generate_number2(length,num1))
 
-def test(data1,data2):
-    lst1=data1
-    lst2=data2
+def test(data1, data2):
+    lst1 = data1
+    lst2 = data2
 
-    bl=BloomFilter(length,probability)
-    print('Size of Bloom Filter:', int(bl.length))
-    print('Optimal number of Hahes for the Bloom Filter:', int(bl.hash_num))
+    bl = BloomFilter(length,probability)
+    print('Size of Bloom Filter: ', int(bl.length))
+    print('Optimal number of Hahes for the Bloom Filter: ', int(bl.hash_num))
 
     for i in range(length):
         bl.insert(lst1[i])
@@ -52,31 +52,30 @@ def test(data1,data2):
     print('Bloom Filter created')
 
     #test
-    count1=0
-    count2=0
-    false_count=0
+    count1 = 0
+    count2 = 0
+    false_count = 0
     for i in range(len(lst1)):
-        if bl.verify(lst1[i])==True:
-           
-            count1+=1
-    if count1==len(lst1):
+        if bl.verify(lst1[i]) == True:
+            count1 += 1
+    if count1 == len(lst1):
         print('Present Data passed')
     
     for i in range(len(lst2)):
-        if bl.verify(lst2[i])==False:
-            count2+=1
+        if bl.verify(lst2[i]) == False:
+            count2 += 1
         else:
             print('False Positive: ', lst2[i], bl.bit_vector)
-            false_count+=1
-    if count2==len(lst2):
+            false_count += 1
+    if count2 == len(lst2):
         print('Non-present Data passed')
     else:
         print("The False Positives have been printed above and they are: ",false_count)
 
-test(num1,num2)
+test(num1, num2)
 
 def gen_sentence_data(length):
-    lst=[]
+    lst = []
     for i in range(length):
         s = RandomSentence()
         lst.append(s.sentence())

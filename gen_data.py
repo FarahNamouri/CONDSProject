6e39b4abe261583d75f6a066b776b36925d56e03
FaisalSherKaizer <faisalsher801@gmail.com>
2024-09-01 00:57:28 +0000

    Data Generation for DNA sequences
    this python file is for the generation of data for DNA sequences so that testing can be performed.

---
+++ gen_data.py
@@ -0,0 +1,15 @@
+#!usr/bin/env/ pyhton
+# importing necessary packages
+import math
+import mmh3
+from hashlib import sha256
+import hmac,hashlib,io
+from hashlib import pbkdf2_hmac
+from bitarray import bitarray
+from BloomFilter import BloomFilter
+import numpy as np
+import random
+from wonderwords import RandomSentence
+import time
+import matplotlib.pyplot as plt
+import json
